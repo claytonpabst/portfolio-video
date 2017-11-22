14015d95e42c349b14ac41deb98c28c1edc694df
Clayton Pabst <claytonpabst@gmail.com>
2017-11-21 22:49:08 -0700

    added picture and words over pic

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MainHeader from './../Headers/MainHeader.js';
+import WelcomePicture from './WelcomePicture.js';
 
 import './Home.css';
 
@@ -7,14 +8,22 @@ import './Home.css';
 class Home extends Component {
 
   render() {
+
+    let videoStyle = {
+      "width":"100%",
+      "height":"56vw",
+      "top": "0px",
+    }
     return (
       <div className="home">
 
           <MainHeader/>
           <div className='GpageNameHeader'>
             <h1>VIDEOGRAPHY</h1>
-          </div> 
-
+          </div>
+          <WelcomePicture/>
+          {/*<iframe hspace="0" vspace="0" marginheight="0" marginwidth="0" id="player" frameBorder="0" allowfullscreen="1" gesture="media" title="YouTube video player" src="https://www.youtube.com/embed/B1rmCjzkFJ8?autohide=1&amp;autoplay=1&amp;controls=0&amp;mute=1&amp;enablejsapi=1&amp;iv_load_policy=3&amp;loop=0&amp;modestbranding=1&amp;playsinline=1&amp;rel=0&amp;showinfo=0&amp;wmode=opaque&amp;origin=https%3A%2F%2Fjustinography.com&amp;widgetid=1" class="backgroundVideo" style={videoStyle}></iframe>*/}
+          {/*<iframe src="https://drive.google.com/file/d/1f3sFeixBMjcJagsCv2E39K6VqeulhyhX/preview" style={videoStyle}></iframe>*/}
       </div>
     );
   }
---
+++ src/components/Home/WelcomePicture.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './WelcomePicture.css';
+
+export default function WelcomePicture(){
+
+
+  return(
+    <div className="welcomePictureWrapper">
+      <section className='welcomePictureBackground'></section>
+      <div className="welcomePictureText">
+        <h1>Hi, I'm Clayton</h1>
+        <h3>and I make videos</h3>
+        <h4>Business/Promotion | Band/Artist | Wedding/First Look | Event</h4>
+        <h5 to='/' style={{"border":"3px solid black"}} className='findOutMoreButton'>FIND OUT MORE</h5>
+      </div>
+    </div>
+  )
+}
